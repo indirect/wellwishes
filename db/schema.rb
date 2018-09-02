86181b30a247c0510600e679b8e347614d59d911
Andre Arko <andre@arko.net>
2018-09-02 00:02:23 -0700

    person scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_065009) do
+ActiveRecord::Schema.define(version: 2018_09_02_070141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "wishes", force: :cascade do |t|
     t.bigint "person_id"
