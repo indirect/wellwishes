3b3b146f405bbc0bf72ea7a8d265cd79a1e715d3
Andre Arko <andre@arko.net>
2018-09-01 23:58:23 -0700

    pg database, wishes scaffold

---
+++ app/views/wishes/_wish.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! wish, :id, :string, :belongs_to, :date, :string, :created_at, :updated_at
+json.url wish_url(wish, format: :json)
--- gems.rb
+++ gems.rb
@@ -1,40 +1,20 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 ruby Pathname.new(__dir__).join(".ruby-version").read
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.5.1'
+gem "rails", "~> 5.2.1"
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.1'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
-# Use Puma as the app server
-gem 'puma', '~> 3.11'
-# Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
-gem 'webpacker'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.5'
-# Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 4.0'
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use ActiveStorage variant
-# gem 'mini_magick', '~> 4.8'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
-# Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', '>= 1.1.0', require: false
+gem "bootsnap", ">= 1.1.0", require: false
+gem "jbuilder", "~> 2.5"
+gem "puma", "~> 3.11"
+gem "pg", "~> 1.1"
+gem "webpacker", "~> 3.5"
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem "pry-byebug", "~> 3.6.0"
 end
 
 group :development do
-  # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'web-console', '>= 3.3.0'
-  gem 'listen', '>= 3.0.5', '< 3.2'
+  gem "web-console", ">= 3.3.0"
+  gem "listen", ">= 3.0.5", "< 3.2"
 end
